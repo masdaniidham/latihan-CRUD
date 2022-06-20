365c2735a5806abd23a1306764cb4011ff66c14e
Masdani Idham Prasojo Karmawan <80078038+masdaniidham@users.noreply.github.com>
2022-06-20 23:55:33 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!--Bootstrap JS-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+    crossorigin="anonymous"></script>
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="app.css">
+
+  <title>About Me</title>
+</head>
+
+<body background="img/bg.png">
+  <!--Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-light py-0 fixed-top" id="mainNavBar">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand">Bakuzan</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navLinks"
+        aria-label="toggle navigation">
+        <span class="navbar-toggler-icon" id="hamburgerIcon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navLinks">
+        <ul class="navbar-nav">
+          <!-- <li class="nav-item">
+            <a href="" class="nav-link">Home</a>
+          </li> -->
+         
+        
+      </div>
+    </div>
+  </nav>
+
+  <!--Content-1-->
+  <div style="background-color: #fff">
+  <section class="container-fluid px-0" >
+    <div class="row align-items-center text-center">
+      <div class="col-lg-6 px-0">
+        <img src="E:\Studies\Web Development\Colt Bootcamp\Museum of Candy\13_Museum_Of_Candy\Starter\imgs\a.jpg" alt=""
+          class="img-fluid">
+      </div>
+      <div class="col-lg-6 px-0">
+        <div class="row justify-content-center">
+          <div class="col-10 mt-5 mt-lg-0">
+            <div>
+              <h2>My Future Plan</h2>
+              <img class="d-none d-lg-inline"
+                src="E:\Studies\Web Development\Colt Bootcamp\Museum of Candy\13_Museum_Of_Candy\Starter\imgs\controller.png"
+                alt="">
+              <p class="my-0 content-paragraph">I have to be a programmer. I need Money duhh!
+              </p>
+              <span class="small-text">Why is it so Hard to learn code, why can't they just make it easy?!</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+  <!--Content-2-->
+  <div style="background-color: #fff">
+  <section class="container-fluid px-0">
+    <div class="row align-items-center text-center content">
+      <div class="col-lg-6 px-0 order-lg-2">
+        <img src="E:\Studies\Web Development\Colt Bootcamp\Museum of Candy\13_Museum_Of_Candy\Starter\imgs\b.jpg" alt=""
+          class="img-fluid">
+      </div>
+      <div class="col-lg-6">
+        <div class="row justify-content-center">
+          <div class="col-10 mt-5 mt-lg-0">
+            <div>
+              <h2>My Dreams</h2>
+              <img class="d-none d-lg-inline"
+                src="E:\Studies\Web Development\Colt Bootcamp\Museum of Candy\13_Museum_Of_Candy\Starter\imgs\controller.png"
+                alt="">
+              <p class="my-0 content-paragraph">Is to work at home and gains a lot of money as a programmer Java specially
+              </p>
+              <span class="small-text">Also to make my family proud!</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+  <!--Content-3-->
+  <div style="background-color: #fff">
+  <section class="container-fluid px-0 section3">
+    <div class="row align-items-center text-center content">
+      <div class="col-lg-6 px-0">
+        <img src="E:\Studies\Web Development\Colt Bootcamp\Museum of Candy\13_Museum_Of_Candy\Starter\imgs\f.jpg" alt=""
+          class="img-fluid">
+      </div>
+      <div class="col-lg-6">
+        <div class="row justify-content-center">
+          <div class="col-10 mt-5 mt-lg-0">
+            <div>
+              <h2>My Identity</h2>
+              <img class="d-none d-lg-inline"
+                src="E:\Studies\Web Development\Colt Bootcamp\Museum of Candy\13_Museum_Of_Candy\Starter\imgs\controller.png"
+                alt="">
+              <p class="my-0 content-paragraph">My Name is Masdani Idham and I'am a 3 third year college student and i'm beginning my journey to become a programmer!
+              </p>
+              <span class="small-text"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ db_bukawarung.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 22, 2022 at 08:45 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bukawarung`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `admin_telp` varchar(20) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`admin_id`, `admin_name`, `username`, `password`, `admin_telp`, `admin_email`, `admin_address`) VALUES
+(0, 'Masdani Idham Prasojo Karmawan', 'admin', '21232f297a57a5a743894a0e4a801fc3', '08111173776', 'idhammasdani@gmail.com', 'Jl. Sawangan Permai, No:53, RT/02 RW/09,Sawangan Baru, Sawangan, Depok, Jawa Barat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_category`
+--
+
+CREATE TABLE `tb_category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_category`
+--
+
+INSERT INTO `tb_category` (`category_id`, `category_name`) VALUES
+(7, 'Baju Anime'),
+(8, 'Celana'),
+(9, 'Makanan'),
+(12, 'Mainan'),
+(13, 'Laptop');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_product`
+--
+
+CREATE TABLE `tb_product` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `product_description` text NOT NULL,
+  `product_image` varchar(100) NOT NULL,
+  `product_status` tinyint(1) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_product`
+--
+
+INSERT INTO `tb_product` (`product_id`, `category_id`, `product_name`, `product_price`, `product_description`, `product_image`, `product_status`, `date_created`) VALUES
+(13, 8, 'jeans', 100000, '<p>bagusss</p>\r\n', 'produk1641827150.jpg', 1, '2022-01-10 15:05:50'),
+(14, 9, 'cireng', 50000, '<p>enak bngttt</p>\r\n', 'produk1641827192.jpg', 1, '2022-01-10 15:06:32'),
+(15, 7, 'baju naruto', 555000, '<p>murah meriah mantep</p>\r\n', 'produk1641827236.png', 1, '2022-01-10 15:07:16'),
+(16, 12, 'action figure', 1000000, '<p>action figure incredible</p>\r\n', 'produk1641828244.jpg', 1, '2022-01-10 15:24:04'),
+(18, 13, 'Asus ROG', 3000000, '<p>spec bagus + ori</p>\r\n', 'produk1641828467.jpg', 1, '2022-01-10 15:27:47');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `tb_category`
+--
+ALTER TABLE `tb_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_category`
+--
+ALTER TABLE `tb_category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
